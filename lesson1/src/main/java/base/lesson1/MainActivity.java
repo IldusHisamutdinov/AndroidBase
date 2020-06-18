cb68c0429642c2f3876416d95c3ce506850e0fac
Developer <aelov62@mail.ru>
2020-06-18 17:40:37 +0500

    добавил layout city_selection.xml

--- lesson1/src/main/java/base/lesson1/MainActivity.java
+++ lesson1/src/main/java/base/lesson1/MainActivity.java
@@ -1,14 +1,18 @@
 package base.lesson1;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.city_selection);
+
+
+
     }
 }
