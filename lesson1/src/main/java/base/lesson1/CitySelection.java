b7577e984b13ec51f58265162206917466ff3f49
Developer <aelov62@mail.ru>
2020-06-21 21:49:03 +0500

    lesson3

--- lesson1/src/main/java/base/lesson1/CitySelection.java
+++ lesson1/src/main/java/base/lesson1/CitySelection.java
@@ -1,14 +1,88 @@
 package base.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CitySelection extends AppCompatActivity {
+    private final String LIFECYCLE = "LIFE_CYCLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_selection);
 
+        ListView listView = findViewById(R.id.listView);
+        String[] arrayCity = getResources().getStringArray(R.array.city);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, arrayCity);
+
+        listView.setAdapter(adapter);
+        String instanceState;
+        if (savedInstanceState == null) {
+            instanceState = "Первый запуск!";
+        } else {
+            instanceState = "Повторный запуск!";
+        }
+        makeToast(instanceState + " onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        makeToast("onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+        makeToast("Повторный запуск!! - onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeToast("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        makeToast("onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        makeToast("Сохранение данных onSaveInstanceState");
+    }
+
+    private void makeToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Log.d(LIFECYCLE, message);
+    }
+
 }
