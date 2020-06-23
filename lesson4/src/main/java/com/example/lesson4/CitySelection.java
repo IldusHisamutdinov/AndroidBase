aa3f68be404429166c13b3284f64ab17771196d7
Developer <aelov62@mail.ru>
2020-06-23 11:45:51 +0500

    lesson4

---
+++ lesson4/src/main/java/com/example/lesson4/CitySelection.java
@@ -0,0 +1,27 @@
+package com.example.lesson4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class CitySelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_selection);
+
+        ListView listView = findViewById(R.id.listView);
+        String[] arrayCity = getResources().getStringArray(R.array.city);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, arrayCity);
+
+        listView.setAdapter(adapter);
+
+    }
+
+}
