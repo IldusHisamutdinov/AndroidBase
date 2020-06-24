35514efb781fde5e17c73c1fad9ddf12b6344ffe
Developer <aelov62@mail.ru>
2020-06-24 21:15:57 +0500

    lesson4

--- lesson4/src/main/java/com/example/lesson4/CitySelection.java
+++ lesson4/src/main/java/com/example/lesson4/CitySelection.java
@@ -2,17 +2,36 @@ package com.example.lesson4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import static com.example.lesson4.R.mipmap.w12_1;
 
 
-public class CitySelection extends AppCompatActivity {
+public class CitySelection extends AppCompatActivity implements Constants {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_selection);
+
+        final EditText text = findViewById(R.id.selection_city);
+        Button startAct = findViewById(R.id.button2);
+        startAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra(TEXT, text.getText().toString());
+                startActivity(intent);
+            }
+        });
 
         ListView listView = findViewById(R.id.listView);
         String[] arrayCity = getResources().getStringArray(R.array.city);
@@ -20,6 +39,7 @@ public class CitySelection extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, arrayCity);
 
         listView.setAdapter(adapter);
+
 
     }
 
--- lesson4/src/main/java/com/example/lesson4/MainActivity.java
+++ lesson4/src/main/java/com/example/lesson4/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.lesson4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements Constants {
+
+    private String city = "Ufa";
+    private static final String CITY = "city";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String text = getIntent().getStringExtra(TEXT);
+        TextView textView = findViewById(R.id.city);
+        textView.setText(text);
+
+        Button startAct = findViewById(R.id.button);
+        startAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CitySelection.class);
+                startActivity(intent);
+            }
+        });
+
+        Button go = findViewById(R.id.button3);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(SITE));
+                startActivity(browserIntent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("CITY", city);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+        city = saveInstanceState.getString("CITY");
     }
 }
