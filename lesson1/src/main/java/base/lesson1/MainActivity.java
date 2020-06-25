285588797f263f7e35fe0ddbe47909dc77145f4a
Developer <aelov62@mail.ru>
2020-06-25 12:59:30 +0500

    Merge branch 'master' of C:\Users\Developer\AndroidStudioProjects\HelloActivity with conflicts.

--- lesson1/src/main/java/base/lesson1/MainActivity.java
+++ lesson1/src/main/java/base/lesson1/MainActivity.java
@@ -1,6 +1,10 @@
 package base.lesson1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
