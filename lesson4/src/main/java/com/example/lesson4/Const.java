43101c4082e090cad5ff76b5c1887af5d5969737
Developer <aelov62@mail.ru>
2020-07-03 10:52:38 +0500

    удалил Const TEXT

--- lesson4/src/main/java/com/example/lesson4/Const.java
+++ lesson4/src/main/java/com/example/lesson4/Const.java
@@ -2,6 +2,5 @@ package com.example.lesson4;
 
 public class Const {
 
-    public static final String TEXT = "TEXT";
     public static final String SITE = "https://www.gismeteo.ru/weather-ufa-4588/";
 }
